0d96027ca575ca7513e718d089c8e5a2399ed35c
woodyfine <1093520060@qq.com>
2019-09-17 15:44:24 +0800

    init

---
+++ src/main/java/com/woody/aic/AicApplication.java
@@ -0,0 +1,13 @@
+package com.woody.aic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woody/aic/AicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woody.aic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
